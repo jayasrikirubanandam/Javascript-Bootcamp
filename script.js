42de18c119a295186a2010bfdc6af379b84891ec
jayasrikirubanandam <kirubajayasri@gmail.com>
2025-03-17 12:38:27 -0400

    git commit -m "Initial commit" git push -u origin main

---
+++ script.js
@@ -0,0 +1,11 @@
+// learning javascript
+
+let amount = 12
+console.log(amount) 
+//var is old way of declaring variables
+//let is new way of declaring variables
+//const is used for constant variables  which cannot be changed later
+//const amount = 12
+//amount = 13
+//console.log(amount) //error
+//javascript is dynamically typed language - no need to specify type of variable
